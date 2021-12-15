c78128e99067c81618a00b5a917da0d1cd6bec8e
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-12-15 09:07:34 +0530

    Added SavingsAccount class to calculate the savings with the monthly interest

--- SavingsAccountProblem/demo/AccountHolder.java
+++ SavingsAccountProblem/demo/AccountHolder.java
@@ -9,16 +9,16 @@ public class AccountHolder {
 		SavingsAccount saver1 = new SavingsAccount(2000);
 		SavingsAccount saver2 = new SavingsAccount(3000);
 		
-		int setAnnualInterestRate = 4;
-		SavingsAccount.annualInterestRate = setAnnualInterestRate;
+		int setAnnualInterestRate = 4;		// defining the annual interest rate
+		SavingsAccount.annualInterestRate = setAnnualInterestRate;		// setting the annual interest rate
 
-		System.out.println("Monthly savings for saver1 with Annual Interest = 4% "+saver1.calculateMonthlyInterest());
+		System.out.println("Monthly savings for saver1 with Annual Interest = 4% "+saver1.calculateMonthlyInterest());		// calculating and printing the savings
 		System.out.println("Monthly savings for saver2 with Annual Interest = 4% "+saver2.calculateMonthlyInterest());
 		
 		setAnnualInterestRate =5;
-		SavingsAccount.modifyInterestRate(setAnnualInterestRate);
+		SavingsAccount.modifyInterestRate(setAnnualInterestRate);		// modifying the interest rate
 		
-		System.out.println("Monthly savings for saver1 with Annual Interest = 5% "+saver1.calculateMonthlyInterest());
+		System.out.println("Monthly savings for saver1 with Annual Interest = 5% "+saver1.calculateMonthlyInterest());		// calculating and printing the savings with modified rate
 		System.out.println("Monthly savings for saver1 with Annual Interest = 5% "+saver2.calculateMonthlyInterest());
 
 	}
--- SavingsAccountProblem/entity/SavingsAccount.java
+++ SavingsAccountProblem/entity/SavingsAccount.java
@@ -2,19 +2,19 @@ package Workshop_2.SavingsAccountProblem.entity;
 
 public class SavingsAccount {
 	
-		public static int annualInterestRate;
-		private int savingsBalance;
+		public static int annualInterestRate;		// declaring annual interest rate common for all the objects
+		private int savingsBalance;					// declaring savings balance as private
 
-		public SavingsAccount(int savingsBalance){
+		public SavingsAccount(int savingsBalance){	// constructor to take value of savings balance
 		this.savingsBalance = savingsBalance;
 		}
 
-		public int calculateMonthlyInterest(){
+		public int calculateMonthlyInterest(){		// calculating the savings with monthly interest
 		savingsBalance = savingsBalance + (savingsBalance*annualInterestRate)/1200;
 		return savingsBalance;
 		}
 
-		public static void modifyInterestRate(int annualInterestRate){
+		public static void modifyInterestRate(int annualInterestRate){		// method to modify the Interest Rate for all the accounts
 		 SavingsAccount.annualInterestRate = annualInterestRate;
 		}
 
